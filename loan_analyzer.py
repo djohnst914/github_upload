42d13e91e58739b11c2e736e9fd8713d4214f088
Dylan Johnston <dylanhjjohnston@gmail.com>
2023-03-19 17:54:45 -0700

    Loop through loans list
    Loop through loans list to find inexpensive loans to add to new list

--- loan_analyzer.py
+++ loan_analyzer.py
@@ -132,6 +132,7 @@ print(f"The present value of the loan is ${present_value:.2f}")
 
 
 # """Part 4: Conditionally filter lists of loans.
+print("Part 4 -->")
 
 # In this section, you will use a loop to iterate through a series of loans and select only the inexpensive loans.
 
@@ -142,56 +143,60 @@ print(f"The present value of the loan is ${present_value:.2f}")
 # 3. Print the list of inexpensive_loans.
 # """
 
-# loans = [
-#     {
-#         "loan_price": 700,
-#         "remaining_months": 9,
-#         "repayment_interval": "monthly",
-#         "future_value": 1000,
-#     },
-#     {
-#         "loan_price": 500,
-#         "remaining_months": 13,
-#         "repayment_interval": "bullet",
-#         "future_value": 1000,
-#     },
-#     {
-#         "loan_price": 200,
-#         "remaining_months": 16,
-#         "repayment_interval": "bullet",
-#         "future_value": 1000,
-#     },
-#     {
-#         "loan_price": 900,
-#         "remaining_months": 16,
-#         "repayment_interval": "bullet",
-#         "future_value": 1000,
-#     },
-# ]
+loans = [
+    {
+        "loan_price": 700,
+        "remaining_months": 9,
+        "repayment_interval": "monthly",
+        "future_value": 1000,
+    },
+    {
+        "loan_price": 500,
+        "remaining_months": 13,
+        "repayment_interval": "bullet",
+        "future_value": 1000,
+    },
+    {
+        "loan_price": 200,
+        "remaining_months": 16,
+        "repayment_interval": "bullet",
+        "future_value": 1000,
+    },
+    {
+        "loan_price": 900,
+        "remaining_months": 16,
+        "repayment_interval": "bullet",
+        "future_value": 1000,
+    },
+]
 
 # @TODO: Create an empty list called `inexpensive_loans`
-# YOUR CODE HERE!
+inexpensive_loans = []
 
 # @TODO: Loop through all the loans and append any that cost $500 or less to the `inexpensive_loans` list
-# YOUR CODE HERE!
+
+for loan in loans:
+    loan_price = loan.get("loan_price")
+    if loan_price <= 500:
+        inexpensive_loans.append(loan_price)
 
 # @TODO: Print the `inexpensive_loans` list
-# YOUR CODE HERE!
+print("Inexpensive loans:", inexpensive_loans)
 
 
-"""Part 5: Save the results.
+# """Part 5: Save the results.
 
-Output this list of inexpensive loans to a csv file
-    1. Use `with open` to open a new CSV file.
-        a. Create a `csvwriter` using the `csv` library.
-        b. Use the new csvwriter to write the header variable as the first row.
-        c. Use a for loop to iterate through each loan in `inexpensive_loans`.
-            i. Use the csvwriter to write the `loan.values()` to a row in the CSV file.
+# Output this list of inexpensive loans to a csv file
+#     1. Use `with open` to open a new CSV file.
+#         a. Create a `csvwriter` using the `csv` library.
+#         b. Use the new csvwriter to write the header variable as the first row.
+#         c. Use a for loop to iterate through each loan in `inexpensive_loans`.
+#             i. Use the csvwriter to write the `loan.values()` to a row in the CSV file.
 
-    Hint: Refer to the official documentation for the csv library.
-    https://docs.python.org/3/library/csv.html#writer-objects
+#     Hint: Refer to the official documentation for the csv library.
+#     https://docs.python.org/3/library/csv.html#writer-objects
 
-"""
+# """
 
 # Set the output header
 # header = ["loan_price", "remaining_months", "repayment_interval", "future_value"]
